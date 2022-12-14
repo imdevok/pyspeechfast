60a227f0ed69841a4fbebbc82f2f1655c6718259
imdevok <lenyamenshov@yandex.ru>
2022-12-14 15:42:18 +0300

    Add concurrent resample with asyncio

--- SpeechFast/converter.py
+++ SpeechFast/converter.py
@@ -1,19 +1,18 @@
-import subprocess
-
+import asyncio
+from subprocess import PIPE
 
 class MediaConverter:
     @staticmethod
-    def execute_ffmpeg_command(cmd: str):
-        ffmpeg_path = r"C:\\ffmpeg\\bin\\ffmpeg.exe"
-        cmd = f"{ffmpeg_path} {cmd}"
-        subprocess.Popen([cmd])
-    
-    @staticmethod
-    def from_any_to_wav(input_file: str, output_file: str):
-        MediaConverter.execute_ffmpeg_command(f"-i {input_file} -acodec pcm_s16le -ac 1 -ar 16000 {output_file}")
+    async def execute_ffmpeg_command(cmd: str):
+        cmd = f"ffmpeg -nostdin -loglevel quiet {cmd}"
+        return await asyncio.create_subprocess_shell(cmd, stdout=PIPE)
 
     @staticmethod
-    def from_mp4_to_wav(input_file: str, output_file: str):
-        MediaConverter.execute_ffmpeg_command(f"-i {input_file} -vn -acodec copy {output_file}")
+    async def from_any_to_wav(input_file: str, output_file: str):
+        return await MediaConverter.execute_ffmpeg_command(f"-i {input_file} -acodec pcm_s16le -ac 1 -ar 16000 {output_file}")
+
+    @staticmethod
+    async def from_mp4_to_wav(input_file: str, output_file: str):
+        return await MediaConverter.execute_ffmpeg_command(f"-i {input_file} -vn -acodec copy {output_file}")
 
 
