db658582e8a491d0eb6874c1dc2abcdd53aaff73
imdevok <lenyamenshov@yandex.ru>
2022-12-14 18:18:30 +0300

    Fix converter

--- SpeechFast/converter.py
+++ SpeechFast/converter.py
@@ -1,18 +1,18 @@
 import asyncio
-
+from subprocess import PIPE, STDOUT
 
 class MediaConverter:
     @staticmethod
     async def execute_ffmpeg_command(cmd: str):
         cmd = f"ffmpeg -nostdin -loglevel quiet {cmd}"
+        process = await asyncio.create_subprocess_shell(cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+        await process.wait()
 
-        return await asyncio.create_subprocess_shell(cmd)
-            
 
     @staticmethod
     async def from_any_to_wav(input_file: str, output_file: str):
-        return await MediaConverter.execute_ffmpeg_command(f"-i {input_file} -acodec pcm_s16le -ac 1 -ar 16000 {output_file}")
+        await MediaConverter.execute_ffmpeg_command(f"-i {input_file} -acodec pcm_s16le -ac 1 -ar 16000 {output_file}")
 
     @staticmethod
     async def from_mp4_to_wav(input_file: str, output_file: str):
-        return await MediaConverter.execute_ffmpeg_command(f"-i {input_file} -vn -acodec copy {output_file}")
+        await MediaConverter.execute_ffmpeg_command(f"-i {input_file} -vn -acodec copy {output_file}")
