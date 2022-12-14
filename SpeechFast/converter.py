ce687e9c44ddafc57b4cf257ecb489f669877b18
imdevok <lenyamenshov@yandex.ru>
2022-12-14 19:06:11 +0300

    Change

--- SpeechFast/converter.py
+++ SpeechFast/converter.py
@@ -14,5 +14,5 @@ class MediaConverter:
         await MediaConverter.execute_ffmpeg_command(f"-i {input_file} -acodec pcm_s16le -ac 1 -ar 16000 {output_file}")
 
     @staticmethod
-    async def from_mp4_to_wav(input_file: str, output_file: str):
-        await MediaConverter.execute_ffmpeg_command(f"-i {input_file} -map 0:a:3 -c copy {output_file}")
+    async def extract_audio(input_file: str, output_file: str):
+        await MediaConverter.execute_ffmpeg_command(f"-i {input_file} -map 0:a {output_file}")
