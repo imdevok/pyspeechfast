3dd3f551f8b9c24a3ed16a4cb272b49cc95bcb1e
imdevok <lenyamenshov@yandex.ru>
2022-12-16 17:57:34 +0300

    Fix bug 'No such file or directory'

--- SpeechFast/converter.py
+++ SpeechFast/converter.py
@@ -1,18 +1,21 @@
-import asyncio
-from subprocess import PIPE, STDOUT
+import subprocess
+import shlex
+import os
+
 
 class MediaConverter:
     @staticmethod
-    async def execute_ffmpeg_command(cmd: str):
-        cmd = f"ffmpeg -nostdin -loglevel quiet {cmd}"
-        process = await asyncio.create_subprocess_shell(cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
-        await process.wait()
-
-
-    @staticmethod
-    async def from_any_to_wav(input_file: str, output_file: str):
-        await MediaConverter.execute_ffmpeg_command(f"-i {input_file} -acodec pcm_s16le -ac 1 -ar 16000 {output_file}")
+    def _execute_ffmpeg_command(cmd: str):
+        cmd = f'ffmpeg {cmd}'
+        try:
+            subprocess.call(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        except FileNotFoundError:
+            raise Exception('Please, install ffmpeg first!')
 
     @staticmethod
-    async def extract_audio(input_file: str, output_file: str):
-        await MediaConverter.execute_ffmpeg_command(f"-i {input_file} -map 0:a {output_file}")
+    def from_any_to_wav(input_file: str, output_file: str):
+        MediaConverter._execute_ffmpeg_command(f'-i "{input_file}" -acodec pcm_s16le -ac 1 -ar 16000 "{output_file}"')
+
+    @staticmethod
+    def extract_audio(input_file: str, output_file: str):
+        MediaConverter._execute_ffmpeg_command(f'-i "{input_file}" -map 0:a "{output_file}"')
